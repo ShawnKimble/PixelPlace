75e0f887386094c254c7e021275a478acfa95156
Chad Auld <chadauld@gmail.com>
2018-02-17 07:57:34 -0700

    Integrating blueprintjs

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react'
 import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from './utils/getWeb3'
 
+//BP test
+import { Spinner, Intent } from "@blueprintjs/core";
+
 import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
+import './css/normalize.css'
+import './css/@blueprintjs/core/dist/blueprint.css'
 import './App.css'
 
 class App extends Component {
@@ -78,6 +83,7 @@ class App extends Component {
           <div className="pure-g">
             <div className="pure-u-1-1">
               <h1>Good to Go!</h1>
+              <Spinner intent={Intent.PRIMARY} />
               <p>Your Truffle Box is installed and ready.</p>
               <h2>Smart Contract Example</h2>
               <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
