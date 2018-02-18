54ec7ea81d391b8e283bd27915b0e415964177e3
David Ashburner <davidashburner@Davids-15in-MacBook-Pro.local>
2018-02-18 00:11:38 -0700

    added new contracts to simpleStorage.

--- contracts/CanvasProperties.sol
+++ contracts/CanvasProperties.sol
@@ -3,56 +3,66 @@ pragma solidity ^0.4.18;
 contract CanvasProperties {
   
     //global properties
-    address CanvasAddress; //public key of the contract/canvas
-    uint PixelChangeCount;
-    uint128 PixelChangePrice;
+    // address CanvasAddress; //public key of the contract/canvas
+    // uint PixelChangeCount;
+    // uint128 PixelChangePrice;
    
+
+   uint storedData;
+      
+    function set(uint x) public {
+      storedData = x;
+    }
+  
+    function get() public view returns (uint) {
+      return 77;
+    }
     
     //this is used to track the pixels on the canvas.
-    struct PixelProperty {
-    uint pixelIndex; //this reperesents the location of the pixel on the canvas.
-    string pixelColor;
-    string pixelStatus;
-    address ownerAddress;
-    }
+    // struct PixelProperty {
+    // uint pixelIndex; //this reperesents the location of the pixel on the canvas.
+    // string pixelColor;
+    // string pixelStatus;
+    // address ownerAddress;
+    // }
     //this specifies the variables for the representation of the phsical canvas.
-    struct CanvasProps {
-        string canvasName;   // Name of the canvas.
-        uint canvasWidth;    // number of pixels for the canvas width.
-        uint canvasHeight;    // number of pixels for the canvas height.
-    }
+    // struct CanvasProps {
+    //     string canvasName;   // Name of the canvas.
+    //     uint canvasWidth;    // number of pixels for the canvas width.
+    //     uint canvasHeight;    // number of pixels for the canvas height.
+    // }
     
     // This declares a state variable that
     // stores a `CanvasProps` struct for each possible address.
-    mapping(address => CanvasProps) public canvas;
+   // mapping(address => CanvasProps) public canvas;
 
 
     // A dynamically-sized array of `PixelProperties` structs.
     //this is our store of the pixels on the canvas and their latest state.
-    PixelProperty[] public pixelProperties;
+    //PixelProperty[] public pixelProperties;
  
     // this is the function that runs first to set up the canvas state.
-     function CanvasProperties(string canvasName, uint canvasWidth, uint canvasHeight) public {    
-        CanvasAddress = msg.sender; 
-        canvas[CanvasAddress].canvasName = canvasName;
-        canvas[CanvasAddress].canvasWidth = canvasWidth;
-        canvas[CanvasAddress].canvasHeight = canvasHeight;
-    }
+    //  function CanvasProperties(string canvasName, uint canvasWidth, uint canvasHeight) public {    
+    //     CanvasAddress = msg.sender; 
+    //     canvas[CanvasAddress].canvasName = "Hello World Yall";
+    //     canvas[CanvasAddress].canvasWidth = canvasWidth;
+    //     canvas[CanvasAddress].canvasHeight = canvasHeight;
+    // }
 
     // This function is used to recieve pixel changes from the front end and update our store(pixelProperties)
     // I am expecting the call of this to be:
     // web3.eth.sendTransaction({from: ..., to: addressOfE, data: {pixelIndex:123, pixelColor: FF0000}}});
-    function UpdatePixels(uint incomingPixelIndex, string incomingPixelColor, string incomingPixelStatus) public {
-      pixelProperties.push(
-        PixelProperty({
-          ownerAddress: msg.sender,
-          pixelIndex : incomingPixelIndex,
-          pixelColor: incomingPixelColor,
-          pixelStatus: incomingPixelStatus
-          }));
-    }
+    // function UpdatePixels(uint incomingPixelIndex, string incomingPixelColor, string incomingPixelStatus) public {
+    //   pixelProperties.Push(
+    //     PixelProperty({
+    //       ownerAddress: msg.sender,
+    //       pixelIndex : incomingPixelIndex,
+    //       pixelColor: incomingPixelColor,
+    //       pixelStatus: incomingPixelStatus
+    //       }));
+    // }
 
-  function get() public view returns (PixelProperty[]) {
-    return pixelProperties;
-  }
+  // function get() public view returns (uint) {
+  //   return 1;
+  // }
 }
