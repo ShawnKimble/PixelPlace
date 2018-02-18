1394d216fb6e11b9ebfbfef5e8f6cb97fadb9e42
Chad Auld <chadauld@gmail.com>
2018-02-17 23:11:38 -0700

    Lots more UI work

--- src/Nav.js
+++ src/Nav.js
@@ -2,10 +2,6 @@
 import React, { Component } from 'react';
 
 class Nav extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
         <nav className="navbar pure-menu pure-menu-horizontal">
